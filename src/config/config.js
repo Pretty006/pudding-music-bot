8233a741a0e5855bf1a984b7f39590af239c5470
Pretty006 <Pretty006@gmail.com>
2026-01-16 07:20:56 +0530

    Added new file

---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  token: "", // your bot token
+  prefix: "=", // prefix
+  color: "#000000", // color
+  Mongo: "",
+  ownerIDS: ["1085459608707014706"],
+  adminIDS: ["1085459608707014706"],
+  vote: true, // false if you dont have api key of DBL
+  invite: "https://discord.gg/XPUqfQvHkx",
+  ssLink: "https://discord.gg/94utkhvpk5",
+  dbl: "https://top.gg/bot/1443190285701087294?s=05235abffca6a",
+  dblToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib3QiOiJ0cnVlIiwiaWQiOiIxNDQzMTkwMjg1NzAxMDg3Mjk0IiwiaWF0IjoiMTc2Nzk3OTI2NCJ9.eyJib3QiOiJ0cnVlIiwiaWQiOiIxNDQzMTkwMjg1NzAxMDg3Mjk0IiwiaWF0IjoiMTc2Nzk3OTI2NCJ9.rZoWIMMo4o1nBAOodXNQQK6a0fn_5qIAb8SgEOz87xg",
+  LOG_CHANNEL: "1460500203172921448",
+  voteChannel: "1459238438393090149",
+  NOPWEB: "https://discord.com/api/webhooks/1443229800595460187/FVFFGSrqGTsXrH_e2TN2vNUGncSl1HTq_w1LowP6y-rIhjnor0JTJ9Dz8XhlzOcj9YYJ",
+  cmd_log: "https://discord.com/api/webhooks/1443229802755784817/biV1mMS5DS6ohgjGG4Bp7GarosQ-tUZqI-ZmAiZEUK05x6MUNY8BRP4D7CjQiPREkI0L",
+  error_log: "https://discord.com/api/webhooks/1443229802885681273/sNDEonkdZRve4w_5EN0swY5S9gRFJnzwmXeiYQlHrfQ7fvFml5AJv1Y4Pil4jD4MmPL1",
+  join_log: "https://discord.com/api/webhooks/1443229803162370153/3FSLBnd0Ym3d2EPGt7c_Z_9Y2RYdvmplktd3sRFZGmj54rotVp4sw3tdN7WW6u0aONKJ",
+  leave_log: "https://discord.com/api/webhooks/1443229804634574941/1se092w2rpe5WaLTM-RBi04O9y8g_MFWp8POtyxiX03wX8GLU4kx78fhaA3wS1Rtw1BV",
+  wel:"https://discord.com/api/webhooks/1452599369227305012/PrzQhK1OB0v_L9QQtVp03zgk-IIzu_MwpiXgo-d3sIB4iiT6dt-k6SROxTpm5vNOn6Iu",
+  spotiId: "85aab1d51a174aad9eed6d7989f530e6",
+  spotiSecret: "b2ad05aa725e434c88776a1be8eab6c2",
+  nodes: [
+    {
+      name: "PUDDING LAVALINK", // your lavalink name [ use v4 lavalink version for that ]
+       url: "lava-v4.ajieblogs.eu.org:443", // url of your lavalink
+       auth: "ajieblogs", // password of your lavalink
+      secure: true, // ssl = true , non ssl = false
+       },
+  ],
+};
